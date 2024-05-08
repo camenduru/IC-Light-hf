93f83a7eb6a91b04732def77fcef6b4e0ad2b61b
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-05-07 18:46:59 -0700

    i

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import torch
+print(f"Is CUDA available: {torch.cuda.is_available()}")
+# True
+print(f"CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+# Tesla T4
+
 import gradio as gr
 
 def greet(name):
